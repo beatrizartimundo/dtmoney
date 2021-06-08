70bff83227506a3cc72531bff57b95a52a6b6946
beatrizartimundo <beatriz.artimundo@gmail.com>
2021-06-08 08:19:21 -0300

    add miragejs and modal component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,33 @@
-import React from "react";
+import {useState} from "react";
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
 import { GlobalStyle } from "./styles/global";
+import { NewTransactionModal} from "./components/NewTransactionModal";
 
+import Modal from 'react-modal'
+
+Modal.setAppElement('#root')
 
 export function App() {
+
+  const [isNewTransactionModalOpen,setIsNewTransactionModalOpen] = useState(false)
+
+    function handleOpenNewTransactionModal(){
+        setIsNewTransactionModalOpen(true)
+    }
+
+    function handleCloseNewTransactionModal(){
+        setIsNewTransactionModalOpen(false)
+    }
   return (
     <>
-      <Header/>
+    {/* repassa a função do App pro header */}
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal}/>
       <Dashboard/>
+      <NewTransactionModal
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTransactionModal}
+      />
       <GlobalStyle/>
     </>
   );
