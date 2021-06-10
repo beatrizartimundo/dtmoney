b4b6f764cbdeb24dec6c6f0ccdfe17f29252bf19
beatrizartimundo <beatriz.artimundo@gmail.com>
2021-06-10 08:20:51 -0300

    modal content and styled

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,5 +1,8 @@
 import Modal from 'react-modal'
-
+import { Container, TransactionTypeContainer } from './styles'
+import closeImg from '../../assets/close.svg'
+import incomeImg from '../../assets/income.svg'
+import outcomeImg from '../../assets/outcome.svg'
 
 interface NewTransactionModalProps {
     isOpen:boolean;
@@ -8,10 +11,54 @@ interface NewTransactionModalProps {
 export function NewTransactionModal({isOpen,onRequestClose}:NewTransactionModalProps){
     return(
         <Modal 
-      isOpen={isOpen}
-      onRequestClose={onRequestClose}
-      >
-      <h2>Cadastrar transação</h2>
+            isOpen={isOpen}
+            onRequestClose={onRequestClose}
+            overlayClassName="react-modal-overlay"
+            className="react-modal-content"
+        >
+            <button 
+                type="button" 
+                onClick={onRequestClose}
+                className="react-modal-close"
+                >
+                <img src={closeImg} alt="Fechar modal"/>
+            </button>
+        <Container>
+            <h2>Cadastrar transação</h2>
+
+            <input
+             placeholder="Titulo"
+             />
+
+             <input
+             placeholder="Valor"
+             />
+
+            <TransactionTypeContainer>
+                <button
+                 type="button"
+                 >
+                     <img src={incomeImg} alt="Entrada"/>
+                     <span>Entrada</span>
+                </button>
+
+                <button
+                 type="button"
+                 >
+                     <img src={outcomeImg} alt="Entrada"/>
+                     <span>Saida</span>
+                </button>
+            </TransactionTypeContainer>
+
+             <input
+             placeholder="Categoria"
+             />
+
+             <button type="submit">
+                 Cadastrar
+            </button>
+        </Container>    
+      
       </Modal>
     )
 }
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,60 +1,99 @@
-import { createGlobalStyle} from 'styled-components'
+import { createGlobalStyle } from 'styled-components'
 
 export const GlobalStyle = createGlobalStyle`
 
-:root{
-    --background: #f0f2f5;
-    --red:#E62E4D;
-    --blue:#5429CC;
+    :root{
+        --background: #f0f2f5;
+        --red:#E62E4D;
+        --blue:#5429CC;
 
-    --blue-light:#6933ff;
+        --blue-light:#6933ff;
+        
+        --text-title: #363F5F;
+        --text-body:#969CB3;
+
+        --green:#33CC95;
+        --shape:#FFFFFF;
+    }
+
+    *{
+        margin:0;
+        padding:0;
+        box-sizing: border-box;
+    }
+
+    html{
+        @media(max-width:1080px){
+            font-size:93.75%;//pega o percentual de acordo a configuração do usuario
+        }
+        @media(max-width:720px){
+            font-size:87.5%;
+        }
+    }
+
+    body {
+        background:var(--background) ;
+        -webkit-font-smoothing: antialiased;
+
+    }
+
+    body,input,textarea,button{
+        font-family: 'Poppins',sans-serif;
+        font-weight: 400;
+    }
+
+    h1,h2,h3,h4,h5,h6,strong{
+        font-family: 'Poppins',sans-serif;
+        font-weight: 600;
+    }
+
+    button { 
+        cursor: pointer;
+    }
+
+    [disabled] {
+        opacity:0.6;
+        cursor: not-allowed;
+    }
+
     
-    --text-title: #363F5F;
-    --text-body:#969CB3;
+    .react-modal-overlay {
 
-    --green:#33CC95;
-    --shape:#FFFFFF;
-}
+        background: rgba(0,0,0,0.5);
 
-*{
-    margin:0;
-    padding:0;
-    box-sizing: border-box;
-}
+        position: fixed;
+        top:0;
+        bottom:0;
+        right:0;
+        left:0;
 
-html{
-    @media(max-width:1080px){
-        font-size:93.75%;//pega o percentual de acordo a configuração do usuario
+        display:flex;
+        align-items:center;
+        justify-content:center;
+
     }
-    @media(max-width:720px){
-        font-size:87.5%;
+
+    .react-modal-content {
+        width:100%;
+        max-width:576px;
+        background: var(--background);
+        padding: 3rem;
+        position: relative;
+        border-radius: 0.24rem;
+
     }
-}
 
-body {
-    background:var(--background) ;
-    -webkit-font-smoothing:antialiased;
+    .react-modal-close{
+        position: absolute;
+        right:1.5rem;
+        top: 1.5rem;
+        border: 0;
+        background: transparent;
 
-}
+        transition: filter 0.2s;
 
-body,input,textarea,button{
-    font-family: 'Poppins',sans-serif;
-    font-weight: 400;
-}
-
-h1,h2,h3,h4,h5,h6,strong{
-    font-family: 'Poppins',sans-serif;
-    font-weight: 600;
-}
-
-button{ 
-    cursor: pointer;
-}
-
-[disabled]{
-    opacity:0.6;
-    cursor:not-allowed;
-}
-
-
+        &:hover{
+            filter: brightness(0.8);
+        }
+    }
 `
